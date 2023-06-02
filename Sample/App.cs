61b4b02d3ce86371994bd487323f0dc5b2eab5da
Artem Levin <leoartemlev@yandex.ru>
2023-06-02 21:35:43 +0500

    create callback for catching activity save instance

---
+++ Sample/App.cs
@@ -0,0 +1,18 @@
+﻿using Android.Runtime;
+using TooLargeTool;
+
+namespace Sample;
+
+[Application]
+public class App : Application
+{
+    public App(System.IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)  
+    {  
+    }  
+    
+    public override void OnCreate()
+    {
+        base.OnCreate();
+        RegisterActivityLifecycleCallbacks(new ActivityCallback());
+    }
+}
