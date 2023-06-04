b42872ec00e8be82233397f2e2803635e50a9b3c
Artem Levin <leoartemlev@yandex.ru>
2023-06-04 18:54:23 +0500

    change example app

---
+++ Sample/SecondActivity.cs
@@ -0,0 +1,12 @@
+﻿namespace Sample;
+
+[Activity]
+public class SecondActivity : Activity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        
+        SetContentView(Resource.Layout.activity_second);
+    }
+}
--- Sample/TestFragment.cs
+++ Sample/TestFragment.cs
@@ -1,11 +1,27 @@
-﻿using Android.Views;
+﻿using Android.Content;
+using Android.Views;
+using TooLargeTool.Extensions;
 
 namespace Sample;
 
-public class TestFragment : Fragment
+public class TestFragment : Fragment, View.IOnClickListener
 {
     public override View? OnCreateView(LayoutInflater? inflater, ViewGroup? container, Bundle? savedInstanceState)
     {
         return inflater.Inflate(Resource.Layout.test_fragment, container, false);
+    }
+
+    public override void OnViewCreated(View? view, Bundle? savedInstanceState)
+    {
+        base.OnViewCreated(view, savedInstanceState);
+        var button = View?.FindViewById<Button>(Resource.Id.startActivityButton);
+        button?.SetOnClickListener(this);
+    }
+
+    public void OnClick(View? v)
+    {
+        var intent = new Intent(Context, typeof(SecondActivity));
+        intent.PutExtra("test_key", "test_data");
+        Activity?.StartActivityWithLoggingExtras(intent);
     }
 }
